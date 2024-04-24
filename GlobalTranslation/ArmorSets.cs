61cbd26f6b950b74ab768aee9927c113336b5f79
moonditch <156866717+moonditch@users.noreply.github.com>
2024-04-24 12:32:58 +0800

    部分更新（缓慢）

--- GlobalTranslation/ArmorSets.cs
+++ GlobalTranslation/ArmorSets.cs
@@ -12,11 +12,22 @@ namespace LunarVeilzhcn.GlobalTranslation
         {
             if (set == "WindmillionHatSet") 
             {
-                player.setBonus = "测试";
+                player.setBonus = "我是万里挑一的\n增加30%的投掷攻速！\n大幅增加投掷击退";
                }
+
+            if (set == "AlsisMaskSet") 
+            {
+                player.setBonus = "你是一个真正的法师........\n获得魔力花和魔法手铐的效果并扩大魔力的拾取范围\n增加25%魔法伤害！\n魔力消耗降低70%并超级的增加魔力值回复";
+               }
+
+            if (set == "ArtisanMaskSet") 
+            {
+                player.setBonus = "3.2.1微笑！就像你站在舞台上好吗:P\n每隔一段时间会获得一段倒计时，倒计时结束后暴击率100%则攻击翻倍\n走路会留下油漆\n暴击率和盔甲穿透增加20！";
+               }
+
             if (set == "WitchenHatSet") 
             {
-                player.setBonus = "测试";
+                player.setBonus = "真是一个敢于奉献的人! 女巫们都非常尊敬你 :P\n获得魔力花和魔法手铐的效果\n极限的增加魔力值回复\n魔力消耗降低50%\n华丽的节奏";
                }
         }
         #endregion
@@ -28,6 +39,19 @@ namespace LunarVeilzhcn.GlobalTranslation
             {
                 return "WindmillionHatSet";
             }
+
+            ModItem item = ModContent.GetInstance<AlsisMask>();
+            if (item.Type == head.type && item.IsArmorSet(head, body, legs))
+            {
+                return "AlsisMaskSet";
+            }
+
+            ModItem item = ModContent.GetInstance<ArtisanMask>();
+            if (item.Type == head.type && item.IsArmorSet(head, body, legs))
+            {
+                return "ArtisanMaskSet";
+            }
+
             item = ModContent.GetInstance<WitchenHat>();
             if (item.Type == head.type && item.IsArmorSet(head, body, legs))
             {
