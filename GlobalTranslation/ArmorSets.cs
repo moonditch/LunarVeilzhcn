dc06d0d646ad03d7da42767adad985ffd518692a
moonditch <156866717+moonditch@users.noreply.github.com>
2024-06-10 10:43:30 +0800

    同步/修改物品汉化到醋+部分套装效果代码

--- GlobalTranslation/ArmorSets.cs
+++ GlobalTranslation/ArmorSets.cs
@@ -4,6 +4,8 @@ using Stellamod.Items.Armors.Windmillion;
 using Stellamod.Items.Armors.Witchen;
 using Stellamod.Items.Armors.Artisan;
 using Stellamod.Items.Armors.Alsis;
+using Stellamod.Items.Armors.Govheil;
+using Stellamod.Items.Armors.Xexterian;
 
 namespace LunarVeilzhcn.GlobalTranslation
 {
@@ -30,6 +32,20 @@ namespace LunarVeilzhcn.GlobalTranslation
             if (set == "WitchenHatSet") 
             {
                 player.setBonus = "真是一个敢于奉献的人! 女巫们都非常尊敬你 :P\n获得魔力花和魔法手铐的效果\n极限的增加魔力值回复\n魔力消耗降低50%\n华丽的节奏";
+               }
+            //忍钢套装效果
+          if (set == "GovheilMaskSet") 
+            {
+                player.setBonus = "敌人可能不会太瞄准你\n忍钢之刃会保护你\n每隔一段时间，魔法和召唤伤害都会增加一次\n最大召唤栏增加2";
+               }
+ /*           if (set == "GovheilHelmetSet") 作者文案错误占位符
+            {
+                player.setBonus = "？\n？\n？\n？";
+               }*/
+
+          if (set == "XexterianHatSet") 
+            {
+                player.setBonus = "拥有沙漠靴的功能\n没有沙漠靴也能快速奔跑\n增加5点防御\n拥有禁戒盔甲的套装效果\n将木箭转化为烈焰箭";
                }
         }
         #endregion
@@ -58,6 +74,23 @@ namespace LunarVeilzhcn.GlobalTranslation
             if (item.Type == head.type && item.IsArmorSet(head, body, legs))
             {
                 return "WitchenHatSet";
+            }
+
+            item = ModContent.GetInstance<GovheilMask>();
+            if (item.Type == head.type && item.IsArmorSet(head, body, legs))
+            {
+                return "GovheilMask";
+            }
+ /*            item = ModContent.GetInstance<GovheilHelmet>();
+            if (item.Type == head.type && item.IsArmorSet(head, body, legs))
+            {
+                return "GovheilHelmet";
+            }*/
+
+            item = ModContent.GetInstance<XexterianHat>();
+            if (item.Type == head.type && item.IsArmorSet(head, body, legs))
+            {
+                return "XexterianHat";
             }
             return base.IsArmorSet(head, body, legs);
         }
--- GlobalTranslation/ItemTooltips.cs
+++ GlobalTranslation/ItemTooltips.cs
@@ -13,6 +13,15 @@ namespace LunarVeilzhcn.GlobalTranslation
 			{
 			//紫色文字
 			tooltipLine.Text = tooltipLine.Text.Replace("Simple Brooch!", "简单的胸针!");
+			//银色星辰文字
+			tooltipLine.Text = tooltipLine.Text.Replace("Can be changed to", "可以变成");
+			//绿色文字
+			tooltipLine.Text = tooltipLine.Text.Replace("(A) Great Damage scaling for explosions!", "(A) 爆炸造成的巨大伤害 !");
+			//红色文字
+			tooltipLine.Text = tooltipLine.Text.Replace("(Special) The farther your cursor is, the faster the axe goes!", "(特殊) 光标越远，斧头砍得越快");
+			//黄色文字
+			tooltipLine.Text = tooltipLine.Text.Replace("(A) Very High Damage Scaling with knives!", "(A) Very High Damage Scaling with knives!（仅测试可无翻译，来自穹月汉化硬翻代码源，以后测试加更新）");
+			tooltipLine.Text = tooltipLine.Text.Replace("(A) Very High Damage Scaling with frost explosions!", "(A) Very High Damage Scaling with frost explosions!（仅测试可无翻译，来自穹月汉化硬翻代码源，以后测试加更新）");
 			}
 		}
 	}
